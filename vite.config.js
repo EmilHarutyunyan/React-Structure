3caeeadc5fdc136ef65d69f1cb1d932b37b6215b
EmilHarutyunyan <emil.h098@gmail.com>
2022-12-31 15:25:25 +0400

    vite && react-compress

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgrPlugin from 'vite-plugin-svgr';
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react(), svgrPlugin()],
+})
